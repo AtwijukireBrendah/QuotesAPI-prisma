c6ecf1d26f955e6dd9072991067815a7577ad93b
AtwijukireBrendah <kire5brenda@gmail.com>
2022-07-12 13:47:55 +0300

    using cors npm

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express=require('express');
 const app = express();
+const cors = require('cors');
 
 //routers
 const quoteRouter = require('./routes/quoteRouter');
 const authorRouter = require('./routes/authorRouter');
+const userRouter = require('./routes/userRouter');
 
 //Middleware
 app.use(express.json());
+app.use(cors());
 
 //Route Handlers
 app.use("/quotes",quoteRouter);
 app.use("/authors",authorRouter);
+app.use("/users",userRouter);
 
 const PORT = process.env.PORT || 5009;
 app.listen(PORT,()=>{
